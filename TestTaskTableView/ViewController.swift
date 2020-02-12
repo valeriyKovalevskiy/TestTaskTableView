511350a3a939a995137607b7ddd61241ec1f4f88
Valeriy Kovalevskiy <kovalevskiy2021@gmail.com>
2020-02-12 14:22:27 +0300

    Initial Commit

---
+++ TestTaskTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskTableView
+//
+//  Created by Valeriy Kovalevskiy on 2/12/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
